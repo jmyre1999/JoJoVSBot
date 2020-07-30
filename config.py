fa927361cd4866e2108737530406ff17bc5e0412
BuildTools <unconfigured@null.spigotmc.org>
2020-07-30 01:13:09 -0400

    removed config from ignore

---
+++ config.py
@@ -0,0 +1,7 @@
+import twitter
+
+def getApi():
+	return twitter.Api(consumer_key=environ['CONSUMER_KEY'], 
+					   consumer_secret=environ['CONSUMER_SECRET'], 
+					   access_token_key=environ['ACCESS_KEY'], 
+					   access_token_secret=environ['ACCESS_SECRET'])
